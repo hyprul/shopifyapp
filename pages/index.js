c5330bcf315d3cf6837741bcc55d79195a458e87
Sean Huang <seanh@ritani.com>
2020-03-23 14:01:33 -0400

    graphql

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { EmptyState, Layout, Page } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
+import { ResourcePicker, TitleBar } from "@shopify/app-bridge-react";
 
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
@@ -10,15 +10,24 @@ class Index extends React.Component {
       <Page>
         <TitleBar
           primaryAction={{
-            content: "Select products"
+            content: "Select products",
+            onAction: () => this.setState({ open: true })
           }}
         />
+        <ResourcePicker
+          resourceType='Product'
+          showVariants={false}
+          open={this.state.open}
+          onSelection={resources => this.handleSelection(resources)}
+          onCancel={() => this.setState({ open: false })}
+        />
+
         <Layout>
           <EmptyState
             heading='Discount your products temporarily'
             action={{
               content: "Select products",
-              onAction: () => console.log("clicked")
+              onAction: () => this.setState({ open: true })
             }}
             image={img}
           >
@@ -28,6 +37,13 @@ class Index extends React.Component {
       </Page>
     );
   }
+
+  handleSelection = resources => {
+    const idsFromResources = resources.selection.map(product => product.id);
+
+    this.setState({ open: false });
+    console.log(idsFromResources);
+  };
 }
 
 export default Index;
